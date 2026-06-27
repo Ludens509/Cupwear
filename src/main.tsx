fdb2602da06001516d85d1949d78f627afa5ab34
Ismael Ludens Alexandre <alexandreludens2@gmail.com>
2026-06-27 14:33:07 -0400

    adding providers to main component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import AppRouter from './routes/router.tsx';
+import { ProductsProvider } from './contexts/products-context/index.tsx';
+import { CartProvider } from './contexts/cartContext/index.tsx';
+import { AuthProvider } from './contexts/authContext/index.ts';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <AppRouter />
+          </CartProvider>
+        </ProductsProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
